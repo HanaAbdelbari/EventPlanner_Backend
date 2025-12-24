c20a7eb821c967bffd6e753dccb285e827415e8f
Basmala Khalifa <elseedybasmala@gmail.com>
2025-12-24 22:30:40 +0200

    Phase3 first commit

--- routes/routes.go
+++ routes/routes.go
@@ -12,7 +12,7 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost", "*"}, //For openshift
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Accept"},
 		ExposeHeaders:    []string{"Content-Length"},
